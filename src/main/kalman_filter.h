86d603b075473bfb45fd72efa0fb5c1aac6ab93a
Leroy Musa <90927915+leroymusa@users.noreply.github.com>
2024-05-19 06:57:17 -0400

    Update kalman_filter.h

--- src/main/kalman_filter.h
+++ src/main/kalman_filter.h
@@ -11,40 +11,37 @@ public:
   void init();
   void begin();
   void update();
-  
-  // Add getter methods for altitude, velocity, and quaternion angles
+
   double getAltitude() const { return filteredAltitude; }
   double getVelocity() const { return filteredVelocity; }
   double getQuaternionW() const { return current_state[2]; }
   double getQuaternionX() const { return current_state[3]; }
   double getQuaternionY() const { return current_state[4]; }
   double getQuaternionZ() const { return current_state[5]; }
+
 private:
   Adafruit_BMP280 *bmp;
   Adafruit_BNO055 *bno;
 
-  // Kalman filter variables
-  double Q[3][3]; // Process noise covariance matrix
+  double Q[6][6]; // Process noise covariance matrix
   double R[2][2]; // Measurement noise covariance matrix
-  double H[2][3]; // Observation matrix
-  double current_p_cov[3][3]; // Current state covariance matrix
-  double A[3][3]; // State transition matrix
+  double H[2][6]; // Observation matrix
+  double current_p_cov[6][6]; // Current state covariance matrix
+  double A[6][6]; // State transition matrix
 
-  double predicted_state[3]; // Predicted state
-  double predicted_p_cov[3][3]; // Predicted state covariance matrix
-  double kalman_gain[3][2]; // Kalman gain matrix
-  double adjusted_state[3]; // Adjusted state
-  double adjusted_p_cov[3][3]; // Adjusted state covariance matrix
+  double predicted_state[6]; // Predicted state
+  double predicted_p_cov[6][6]; // Predicted state covariance matrix
+  double kalman_gain[6][2]; // Kalman gain matrix
+  double adjusted_state[6]; // Adjusted state
+  double adjusted_p_cov[6][6]; // Adjusted state covariance matrix
 
   unsigned long old_time; // Previous time
   unsigned long curr_time; // Current time
   double dt; // Time difference
 
-  // Additional member variables for storing filtered altitude and velocity
   double filteredAltitude;
   double filteredVelocity;
 
-  // State vector: [altitude, velocity, quaternion_w, quaternion_x, quaternion_y, quaternion_z]
   double current_state[6]; // Current state vector
 
   void predict_state();
