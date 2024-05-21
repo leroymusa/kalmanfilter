e9a28e75abb414bd72249b7198df63fcb379e25a
Leroy Musa <90927915+leroymusa@users.noreply.github.com>
2024-05-21 01:58:11 -0400

    Update KalmanFilter.h

--- src/main/KalmanFilter.h
+++ src/main/KalmanFilter.h
@@ -1,25 +1,62 @@
-#ifndef KALMAN_FILTER_H
-#define KALMAN_FILTER_H
+class Kalman {
+public:
+  Kalman(float initialQ_angle, float initialQ_bias, float initialR_measure) {
+    Q_angle = initialQ_angle;
+    Q_bias = initialQ_bias;
+    R_measure = initialR_measure;
 
-// State vector: [altitude, velocity, acceleration]
-extern float state[3];
+    angle = 0;
+    bias = 0;
+    P[0][0] = 0;
+    P[0][1] = 0;
+    P[1][0] = 0;
+    P[1][1] = 0;
+  }
 
-// Covariance matrix
-extern float P[3][3];
+  float getAngle(float newAngle, float newRate, float dt) {
+    rate = newRate - bias;
+    angle += dt * rate;
 
-// Process noise
-extern float Q[3][3];
+    P[0][0] += dt * (dt * P[1][1] - P[0][1] - P[1][0] + Q_angle);
+    P[0][1] -= dt * P[1][1];
+    P[1][0] -= dt * P[1][1];
+    P[1][1] += Q_bias * dt;
 
-// Measurement noise
-extern float R_alt; // Altitude measurement noise
-extern float R_acc; // Acceleration measurement noise
+    float S = P[0][0] + R_measure;
+    float K[2];
+    K[0] = P[0][0] / S;
+    K[1] = P[1][0] / S;
 
-// Kalman gain
-extern float K[3][2];
+    float y = newAngle - angle;
+    angle += K[0] * y;
+    bias += K[1] * y;
 
-// Function declarations
-void kalmanInit();
-void kalmanPredict(float dt);
-void kalmanUpdate(float altitude, float accel);
+    float P00_temp = P[0][0];
+    float P01_temp = P[0][1];
 
-#endif
+    P[0][0] -= K[0] * P00_temp;
+    P[0][1] -= K[0] * P01_temp;
+    P[1][0] -= K[1] * P00_temp;
+    P[1][1] -= K[1] * P01_temp;
+
+    return angle;
+  }
+
+  void setAngle(float newAngle) { angle = newAngle; }
+  float getRate() { return rate; }
+
+  void setQangle(float newQ_angle) { Q_angle = newQ_angle; }
+  void setQbias(float newQ_bias) { Q_bias = newQ_bias; }
+  void setRmeasure(float newR_measure) { R_measure = newR_measure; }
+
+private:
+  float Q_angle;
+  float Q_bias;
+  float R_measure;
+
+  float angle;
+  float bias;
+  float rate;
+
+  float P[2][2];
+};
